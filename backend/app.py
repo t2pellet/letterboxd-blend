1cf1946a64ad51397134b3f46e9b80495c6a1fae
Tenzin Pelletier <tenzin@hivestack.com>
2024-07-18 00:15:48 -0400

    oh my god t deploys

--- backend/app.py
+++ backend/app.py
@@ -3,14 +3,18 @@ from flask import Flask
 from flask_cors import CORS
 from users import users
 from posters import posters
+from waitress import serve
+
 
 app = Flask(__name__)
-CORS(app)
 
 
-app.register_blueprint(users, url_prefix='/users')
-app.register_blueprint(posters, url_prefix='/posters')
+app.register_blueprint(users, url_prefix='/api/users')
+app.register_blueprint(posters, url_prefix='/api/posters')
 
+@app.get("/api")
+def get_test():
+    return 'hi'
 
 if __name__ == '__main__':
-    app.run()
+    serve(app, host='0.0.0.0', port=8080)
