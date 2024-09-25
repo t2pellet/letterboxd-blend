19c94e4d775afaae56f0b2887f27eeba6cee064e
Tenzin Pelletier <tenzin@hivestack.com>
2024-09-25 10:27:16 -0400

    help

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 envsubst '\$PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-nginx -g 'daemon off;' & gunicorn -b '[::1]:8080' --worker-class eventlet -w 1 main:app
+nginx -g 'daemon off;' & gunicorn -b '[::1]:8000' --worker-class eventlet -w 1 main:app
